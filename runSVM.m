c3a5ba9d89530cc9d66d168accd6ad2d0a717ea9
Lorenzo Cioni <lore.cioni@gmail.com>
2015-10-26 20:10:24 +0100

    evaluate sum model on test set

--- runSVM.m
+++ runSVM.m
@@ -11,9 +11,14 @@ addpath(libsvmpath)
 K = configuration.K;
 load(['./mat/signaturesFV_K' int2str(K)]);
 
-image_number = length(labels);
+trainSignatures = signatures(:, 1:100);
+testSignatures = signatures(:, 101:end);
+trainLabels = labels(1:100);
+testLabels = labels(101:end);
 
-% Fit SVM model. Using matlab function for multiclass training
+% SVM training options 
+% SVR model for probability and radial basis function kernel
+% Setting kFold number for cross validation
 kFolds = configuration.kFolds;
 
 % Cross validation
@@ -22,9 +27,10 @@ bestC = 0;
 bestGamma = 0;
 bestAccuracy = 0;
 
-for c = 1e-3 %Testing optimal value [1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e+0 1e+1 1e+2 1e+3 1e+4 1e+5 1e+6]
-    for gamma = [0.8 0.9 1.1 1.5]
-    	acc = svmtrain(labels', signatures', sprintf('-q -b 1 -t 2 -g %f -c %f -v %f',gamma, c, kFolds));
+for c = [1e-3 1e-4 1e-5 1e-6 1e+0 1e+1 1e+2 1e+3 1e+4]
+    for gamma = [0.8 0.9 1.1 1.5]       
+        options = ['-q -b 1 -t 2 -g ' num2str(gamma) ' -c ' num2str(c) ' -v ' num2str(kFolds)];
+    	acc = svmtrain(trainLabels', trainSignatures', options);
         if(acc > bestAccuracy)
         	bestAccuracy = acc;
             bestC = c;
@@ -34,12 +40,20 @@ for c = 1e-3 %Testing optimal value [1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e+0 1e+1 1e+
     end
 end
 
+fprintf('Cross Validation Best Accuracy: %.2f %%\n', bestAccuracy);
+fprintf('Gamma: %.2f\n', bestGamma);
+fprintf('C: %.2f\n', bestC);
+
 % Train the model and test with optimal values of gamma and C
-model = svmtrain(labels', signatures',sprintf('-q -b 1 -t 2 -g %f -c %f', bestGamma, bestC));
-predictedLabels = svmpredict(labels',signatures',model, '-b 1');
+options = ['-q -b 1 -t 2 -g ' num2str(bestGamma) ' -c ' num2str(bestC)];
+model = svmtrain(trainLabels', trainSignatures', options);
+
+
+% Evaluate model (on the same dataset)
+predictedLabels = svmpredict(testLabels', testSignatures', model, '-b 1');
 
 % Generate and plot confusion matrix
-[confusionMatrix, classes] = plotConfusionMatrix(num2classes(labels), num2classes(predictedLabels));
+[confusionMatrix, classes] = plotConfusionMatrix(num2classes(testLabels), num2classes(predictedLabels));
 
 % Evaluate results
 classFrequency = sum(confusionMatrix, 2);
@@ -49,9 +63,11 @@ table(classes, classFrequency, classCorrectedPred, classCorrectRate, ...
     'VariableNames', {'Class', 'Total', 'Correct', 'Rate'})
 
 correctClassifiedCells = sum(classCorrectedPred);
-fprintf('Correct classified cells: %d / %d\n', correctClassifiedCells, image_number);
 
-accuracy = correctClassifiedCells/image_number;
+imageNumber = sum(confusionMatrix(:));
+fprintf('Correct classified cells: %d / %d\n', correctClassifiedCells, imageNumber);
+
+accuracy = correctClassifiedCells/imageNumber;
 fprintf('Accuracy: %.2f %%\n', accuracy * 100);
 
 fprintf('Elapsed time: %.2f s\n\n', etime(clock, start_time));
