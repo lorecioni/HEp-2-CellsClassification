04fe9cb7ffa6d37ef314936ae851cb4e9da4a42d
Lorenzo Cioni <lore.cioni@gmail.com>
2015-12-15 09:52:52 +0100

    config file

--- configuration.m
+++ configuration.m
@@ -3,41 +3,18 @@
 classdef configuration
    properties (Constant)  
        
-       % Set full images or single cells classification (mutually
-       % exclusive)
-       full_images = false;
-       cell_images = true;
-       
        %Paths for train and test dataset
        train_path = 'dataset/train/'; 
        train_labels = 'dataset/trnLabels.mat'; 
        test_path = 'dataset/test/'; 
        test_labels = 'dataset/tstLabels.mat'; 
        
-       %Image path (for full images)
-       full_image_path = 'dataset/images/';  
-       full_image_prefix = 'Siero_';
-       full_image_ext = 'bmp';
-       
-       %Validation file for training
-       % Choose the validation format xls, xlsx, csv (xls not work on Unix,
-       % use xlsx or cvs instead)
-       full_validation_format = 'xlsx';      
-       full_validation_file = 'dataset/Validation_set.xlsx';
-       full_validation_file_worksheet_name = 'Lavoro';
-       full_validation_file_image_ids_column = 3;
-       full_validation_file_image_label_column = 6;
-       
        %Patterns
        patterns = containers.Map( ...
             {1, 2, 3, 4, 5, 6}, ...
             {'homogeneous', 'coarse_speckled', 'fine_speckled', 'centromere', 'nucleolar', 'cytoplasmatic'}...
        );
-   
-       %Full patterns
-       %{1, 2, 3, 5, 6, 7}
-       %{'Omogeneo', 'Punteggiato', 'Nucleolare', 'Citoplasmico', 'Negativo', 'Granulare'}
-              
+
        %Image resize (if set to false image will not be resized)
        resize = true;
        resizeTo = 90;
